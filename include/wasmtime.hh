534a24936d4287a3068d1d2f2978377905ddb21e
Derek Shiell <derek.shiell@verizondigitalmedia.com>
2021-11-03 08:59:31 -0700

    fix comment for Module::deserialize_file()

--- include/wasmtime.hh
+++ include/wasmtime.hh
@@ -1556,8 +1556,9 @@ public:
    * This function is the same as wasmtime_module_deserialize except that it
    * reads the data for the serialized module from the path on disk. This can
    * be faster than the alternative which may require copying the data around.
-   * the artifacts of a previous compilation to quickly create an in-memory
-   * module ready for instantiation.
+   *
+   * This function does not take ownership of any of its arguments, but the
+   * returned error and module are owned by the caller.
    *
    * It is not safe to pass arbitrary input to this function, it is only safe to
    * pass in output from previous calls to `serialize`. For more information see
