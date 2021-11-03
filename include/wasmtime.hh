a4aef406fb6d84337b8b4da2565ed6986190a9ed
Alex Crichton <alex@alexcrichton.com>
2021-11-03 09:18:36 -0500

    Don't use `std::cerr` in header (#21)
    * Don't use `std::cerr` in header
    
    Looks like this isn't always defined and the necessary functionality can
    be done with `fprintf` instead.
    
    cc #18
    
    * Run clang-format
    
    * Don't lint on `fprintf`

--- include/wasmtime.hh
+++ include/wasmtime.hh
@@ -34,6 +34,7 @@
 
 #include <any>
 #include <array>
+#include <cstdio>
 #include <initializer_list>
 #include <iosfwd>
 #include <limits>
@@ -133,7 +134,7 @@ public:
   Error(std::string_view msg) : msg(msg) {}
 
   /// \brief Returns the error message associated with this error.
-  std::string_view message() const { return msg; }
+  const std::string &message() const { return msg; }
 };
 
 /// \brief Used to print an error.
@@ -177,12 +178,12 @@ public:
     if (*this) {
       return this->ok();
     }
-    abort();
+    unwrap_failed();
   }
 
 private:
-  [[noreturn]] void abort() {
-    std::cerr << "error: " << this->err().message() << "\n";
+  [[noreturn]] void unwrap_failed() {
+    fprintf(stderr, "error: %s\n", this->err().message().c_str()); // NOLINT
     std::abort();
   }
 };
