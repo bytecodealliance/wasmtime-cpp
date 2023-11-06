bd45a9b59f255b896ce3f93d385ead1c6567087b
Twice <twice@apache.org>
2023-11-07 03:11:02 +0900

    Make container ctor of Span more general (#44)
    * Make container ctor of Span more general
    
    * fix

--- include/wasmtime.hh
+++ include/wasmtime.hh
@@ -69,7 +69,18 @@ inline constexpr size_t dynamic_extent =
  * @tparam T Type of data
  * @tparam Extent Static size of data refered by Span class
  */
-template <typename T, std::size_t Extent = dynamic_extent> class Span {
+template <typename T, std::size_t Extent = dynamic_extent> class Span;
+
+/// \brief Check whether a type is `Span`
+template <typename T> struct IsSpan : std::false_type {};
+
+template <typename T, std::size_t Extent>
+struct IsSpan<Span<T, Extent>> : std::true_type {};
+
+template <typename T, std::size_t Extent> class Span {
+  static_assert(Extent == dynamic_extent,
+                "The current implementation supports dynamic-extent span only");
+
 public:
   /// \brief Type used to iterate over this span (a raw pointer)
   using iterator = T *;
@@ -78,9 +89,18 @@ public:
   Span(T *t, std::size_t n) : ptr_{t}, size_{n} {}
 
   /// \brief Constructor of Span class for containers
-  template <template <typename, typename> class Container>
-  Span(Container<T, std::allocator<T>> &range)
-      : ptr_{range.data()}, size_{range.size()} {}
+  template <typename C,
+            std::enable_if_t<
+                !IsSpan<C>::value &&
+                    std::is_pointer_v<decltype(std::declval<C &>().data())> &&
+                    std::is_convertible_v<
+                        std::remove_pointer_t<
+                            decltype(std::declval<C &>().data())> (*)[],
+                        T (*)[]> &&
+                    std::is_convertible_v<decltype(std::declval<C>().size()),
+                                          std::size_t>,
+                int> = 0>
+  Span(C &range) : ptr_{range.data()}, size_{range.size()} {}
 
   /// \brief Returns item by index
   T &operator[](ptrdiff_t idx) const {
